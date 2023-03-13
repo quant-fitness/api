b2a488ead1316c7883965bdd2c2be469f5a0e42e
Ryan Morris <ryan.morris@centro.net>
2023-03-13 09:49:08 -0500

    feat(authorization): add the ability to register and sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
+
+  private
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    end
+
+    def store_user_location!
+      store_location_for(:user, request.fullpath)
+    end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@
 FactoryBot.define do
   factory :user do
     email_address { 'user.email@something.org' }
+    password { 'password' }
   end
 end
